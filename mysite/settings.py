be78f5c59ba5afdc6056cb690a3ff6c2f3d396c6
Joel Choi <joelc316@gmail.com>
2021-05-01 18:59:36 -0500

    Debug is False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3)qr_2%wkt2^wshh@dkf2%cv!1w6r8y774%_2bfve*z4q$nq-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
